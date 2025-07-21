20428054fafe413808034af63ed6cac450959fd5
Vladimir Semenov <sticktgt@gmail.com>
2025-07-21 14:47:07 +0200

    added up to 10 levels of graph into the values files

--- template/template.go
+++ template/template.go
@@ -669,12 +669,12 @@ var log = logrus.New()
 func main() {
 	var valuesCheck Values2
 	log.Printf("Reading values file")
-	valData, err := os.ReadFile("../config/values2.json")
+	valData, err := os.ReadFile("../config/values.json")
 	check(err)
 
 	log.Printf("Loading schema file")
-	schemaLoader := gojsonschema.NewReferenceLoader("file://../config/schema2.json")
-	documentLoader := gojsonschema.NewReferenceLoader("file://../config/values2.json")
+	schemaLoader := gojsonschema.NewReferenceLoader("file://../config/schema.json")
+	documentLoader := gojsonschema.NewReferenceLoader("file://../config/values.json")
 	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
 	check(err)
 	if result.Valid() {
@@ -692,7 +692,7 @@ func main() {
 
 	var meta ChartTemplate
 	log.Printf("Reading template file")
-	tmplData, err := os.ReadFile("../config/meta-online-inference-seldon-v2.yaml")
+	tmplData, err := os.ReadFile("../config/meta-online-inference-seldon-v1.yaml")
 	check(err)
 	check(yaml.Unmarshal(tmplData, &meta))
 	/*
